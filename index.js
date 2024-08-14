6df62a0a998f83ccd4c8d4643c77686d4a076776
ankit <ankitxrajpoot@gmail.com>
2024-08-15 01:35:35 +0530

    Added chat logic and chat room

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require("express");
 const http = require('http');
 const socketio = require('socket.io');
 
+const connect = require('./config/database-config');
+const Chat = require('./models/chat');
+
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
@@ -9,17 +12,49 @@ const io = socketio(server);
 io.on('connection', (socket)=>{
     console.log('a new client connected', socket.id);
 
-    socket.on('from client', ()=>{
-        console.log('message from client');
+    socket.on('join_room', (data)=>{
+        console.log('joining room:',data.roomid);
+        socket.join(data.roomid);
     })
 
-    setInterval(()=>{
-        socket.emit('from server');
-    },90000);
+    socket.on('msg_send', async (data)=>{
+        console.log(data);
+        // io.emit('msg_rcvd', data); // all user server recieves msg
+        // socket.emit('msg_rcvd', data) // only sending server responds
+        // socket.broadcast.emit('msg_rcvd', data); // only reciever responds
+        
+        const chat = await Chat.create({
+            content: data.msg,
+            roomId: data.roomid,
+            user: data.username
+        });
+        io.to(data.roomid).emit('msg_rcvd', data);
+    });
+
+    socket.on('typing', (data) => {
+        socket.broadcast.to(data.roomid).emit('someone_typing');
+    });
+
 });
+
+app.set('view engine', 'ejs');
 
 app.use('/', express.static(__dirname + '/public'));
 
-server.listen(3000, ()=>{
+app.get('/chat/:roomid', async (req, res)=>{
+    const chats = await Chat.find({
+        roomid: req.params.roomid
+    }).select('content user');
+    console.log(chats);
+    res.render('index', {
+        name: 'Annn',
+        id: req.params.roomid,
+        chats: chats
+    });
+});
+
+server.listen(3000, async ()=>{
     console.log("Server is running on port 3000");
-})
+    await connect();
+    console.log('mongodb connected');
+});
